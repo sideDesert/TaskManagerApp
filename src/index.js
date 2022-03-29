3f6a49d98e1aabe87ac6c7bc53f2e6efef858418
Siddarth Saha <siddarthsaha1@gmail.com>
2022-03-29 23:34:28 +0530

    Added Updation and Deletion Handler

--- src/index.js
+++ src/index.js
@@ -7,38 +7,35 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res)=>{
+app.post('/users', async (req, res)=>{
     const user = new User(req.body);
-    user.save().then(()=>{
-        console.log(user);
+
+    try{
+        await user.save();
         res.send(user).status(201);
-    }).catch((err)=>{
-        res.send(err).status(400);
-        console.log(err);
-    });
-
+    } catch(e){
+        res.status(400).send(e);
+    }
 });
-app.post('/tasks', (req,res)=>{
+app.post('/tasks', async (req,res)=>{
         const task = new Task(req.body);
-        task.save()
-        .then(()=>{
-            console.log(task);
+        try{
+            await task.save();
             res.send(task).status(201);
-        })
-        .catch((err)=>{
+        } catch(e){
             res.send(err).status(400);
-        });
+        }
 });
-
-app.get('/users', (req,res)=>{
-    User.find({})
-    .then((result)=>{
+//Find all users
+app.get('/users', async (req,res)=>{
+    try{
+        const result = await User.find({});
         res.send(result);
-    })
-    .catch((err)=>{
+
+    } catch(err){
         res.send(err).status(500);
         console.log(err);
-    });
+    }
 });
 app.get('/users/:id', (req,res)=>{
     const _id = req.params.id;
@@ -79,4 +76,62 @@ app.get('/tasks/:id', (req,res)=>{
 
 app.listen(port, ()=>{
     console.log('Server is up on port ' + port);
+});
+
+//Update User
+app.patch('/users/:id', async(req, res )=>{
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['email', 'password', 'age','name'];
+    const isValidOperation = updates.every((update)=>{
+        allowedUpdates.includes(update);
+    });
+    if(!isValidOperation){
+        return res.send({error: 'Invalid Update'})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, });
+        if(!user){
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch(err){
+        res.status(400).send(err);
+    }
+});
+
+//Update Task
+app.patch('/tasks/:id', async(req, res)=>{
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true});
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch(err){
+        res.status(400).send(err);
+    }
+});
+
+//Delete user
+app.delete('/users/:id',async (req, res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            res.status(404).send();
+        }
+        res.send(user);
+    } catch(err){
+        res.status(500).send(err);
+    }
+});
+app.delete('/tasks/:id',async (req, res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task){
+            res.status(404).send();
+        }
+        res.send(task);
+    } catch(err){
+        res.status(500).send(err);
+    }
 });
